00f5c12c92f94d223b13f2696771031774826e70
Ruslan-Shev <134603263+Ruslan-Shev@users.noreply.github.com>
2023-06-29 18:29:42 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class CreditPaymentService {
+public class CreditPaymentService {
+    public double calculate(double amount, double rate, int term) {
+        double i = rate / 12 / 100;
+        double k = i * Math.pow(1 + i, term) / (Math.pow(1 + i, term) - 1);
+        double payment = k * amount;
+        return payment;
+    }
 }
